72bcbab38043476ea31eb0ba5c94993c231e1649
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-20 12:17:18 -0700

    Node complete.

--- node.py
+++ node.py
@@ -1,4 +1,12 @@
 class Node:
   def __init__(self, letter):
-    pass
+    self.letter = letter
+    self.links = {}
+    self.terminator = False
+
+  def add_link(self, letter):
+    self.links[letter] = Node(letter)
+
+  def make_terminator(self):
+    self.terminator = True
 
--- test_node.py
+++ test_node.py
@@ -5,3 +5,31 @@ class NodeTestCase(unittest.TestCase):
 
   def test_it_exists(self):
     assert Node('a')
+
+  def test_node_stores_letter(self):
+    node = Node('a')
+    self.assertEqual('a', node.letter)
+
+  def test_node_initializes_with_empty_links(self):
+    node = Node('a')
+    self.assertEqual({}, node.links)
+
+  def test_node_set_links(self):
+    node = Node('a')
+    node.add_link('b')
+    self.assertEqual('b', node.links['b'].letter)
+
+  def test_node_terminator_defaults_to_false(self):
+    node = Node('a')
+    self.assertFalse(node.terminator)
+
+  def test_node_terminator_set_to_true(self):
+    node = Node('a')
+    node.make_terminator()
+    self.assertTrue(node.terminator)
+
+  def test_node_stores_multiple_linkes(self):
+    node = Node('a')
+    node.add_link('b')
+    node.links['b'].add_link('c')
+    self.assertEqual('c', node.links['b'].links['c'].letter)
